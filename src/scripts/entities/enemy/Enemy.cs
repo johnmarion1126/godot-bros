c4683a7b68bec00581a44bf567d8bd9f1455a7db
John Marion Sanchez <johnmarion1126@gmail.com>
2022-09-11 19:08:49 -0700

    Flip turtle when hitting screen limit

--- src/scripts/entities/enemy/Enemy.cs
+++ src/scripts/entities/enemy/Enemy.cs
@@ -1,22 +1,45 @@
 using Godot;
 using System;
 
+// TODO: Slow down movement speed
+// TODO: Move constants to one file
+// TODO: Flip turtle sprite and direction after hitting wall
+
 public class Enemy : KinematicBody2D
 {
   const float GRAVITY_SCALE =  800.0f;
-  const int WALK_SPEED = 250;
+  int ENEMY_WALK_SPEED = 150;
 
   Vector2 velocity;
+  private AnimatedSprite anim;
 
   public override void _Ready()
   {
+    anim = GetNode<AnimatedSprite>("EnemyAnimation");
     velocity = new Vector2();
   }
 
   public override void _PhysicsProcess(float delta)
   {
-    velocity.x = -WALK_SPEED;
+    if (anim.FlipH) {
+      GD.Print("TRUE");
+      velocity.x = -ENEMY_WALK_SPEED;
+    }
+    else {
+      GD.Print("FALSE");
+      velocity.x = ENEMY_WALK_SPEED;
+    }
+
     velocity.y += delta * GRAVITY_SCALE;
     MoveAndSlide(velocity, new Vector2(0, -1));
+
+    float screenBorder = GetViewport().GetVisibleRect().Size.x;
+
+    if (this.Position.x <= -50) {
+      anim.FlipH = false;
+    } else if (this.Position.x >= screenBorder - 100) {
+      GD.Print("OUT");
+      anim.FlipH = true;
+    }
   }
 }
