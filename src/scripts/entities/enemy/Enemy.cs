c4ce3085775a74edf3bc62f34393d57fdaf3881a
John Marion Sanchez <johnmarion1126@gmail.com>
2022-09-17 18:57:25 -0700

    Use constants for enemy script

--- src/scripts/entities/enemy/Enemy.cs
+++ src/scripts/entities/enemy/Enemy.cs
@@ -4,6 +4,10 @@ public class Enemy : KinematicBody2D
 {
   const float GRAVITY_SCALE =  Constants.GRAVITY_SCALE;
   const int ENEMY_WALK_SPEED = Constants.ENEMY_WALK_SPEED;
+  const int SCREEN_WIDTH = Constants.SCREEN_WIDTH;
+
+  const int PIXEL_LEFT_OFFSET = -50;
+  const int PIXEL_RIGHT_OFFSET = 100;
 
   Vector2 velocity;
   private AnimatedSprite anim;
@@ -22,9 +26,7 @@ public class Enemy : KinematicBody2D
     velocity.y += delta * GRAVITY_SCALE;
     MoveAndSlide(velocity, new Vector2(0, -1));
 
-    float screenBorder = GetViewport().GetVisibleRect().Size.x;
-
-    if (this.Position.x <= -50) anim.FlipH = false;
-    else if (this.Position.x >= screenBorder - 100) anim.FlipH = true;
+    if (this.Position.x <= PIXEL_LEFT_OFFSET) anim.FlipH = false;
+    else if (this.Position.x >= SCREEN_WIDTH - PIXEL_RIGHT_OFFSET) anim.FlipH = true;
   }
 }
