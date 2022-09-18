f061afeb04fcf2718ae7d551b3fe13b34afe4e46
John Marion Sanchez <johnmarion1126@gmail.com>
2022-09-17 18:57:03 -0700

    Fix jump

--- src/scripts/Constants.cs
+++ src/scripts/Constants.cs
@@ -1,7 +1,8 @@
 static class Constants
 {
-  public const float GRAVITY_SCALE = 800.0f;
+  public const float GRAVITY_SCALE = 1500.0f;
+  public const int SCREEN_WIDTH = 450;
   public const int WALK_SPEED = 250;
-  public const int JUMP_SPEED = -450;
+  public const int JUMP_SPEED = -600;
   public const int ENEMY_WALK_SPEED = 150;
 }
