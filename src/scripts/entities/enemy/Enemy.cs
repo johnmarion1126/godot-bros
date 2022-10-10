404bf4466316f62368a8ee057c0e2b45ee35d15e
John Marion Sanchez <johnmarion1126@gmail.com>
2022-10-09 17:31:17 -0700

    Add fainting animation

--- src/scripts/entities/enemy/Enemy.cs
+++ src/scripts/entities/enemy/Enemy.cs
@@ -2,13 +2,16 @@ using Godot;
 using System;
 public class Enemy : KinematicBody2D
 {
-  const float GRAVITY_SCALE =  Constants.GRAVITY_SCALE;
   const int ENEMY_WALK_SPEED = Constants.ENEMY_WALK_SPEED;
   const int SCREEN_WIDTH = Constants.SCREEN_WIDTH;
+  const float GRAVITY_SCALE =  Constants.GRAVITY_SCALE;
+  const float JUMP_SPEED = Constants.JUMP_SPEED;
 
   const int PIXEL_LEFT_OFFSET = -40;
   const int PIXEL_RIGHT_OFFSET = 100;
   const int PIXEL_TOP_OFFSET = 43;
+
+  bool fainted = false;
 
   Vector2 velocity;
   private AnimatedSprite anim;
@@ -22,7 +25,14 @@ public class Enemy : KinematicBody2D
   }
 
   public void onCollisionEnter(Area2D area) {
-    if (area.Position.y <= this.Position.y - PIXEL_TOP_OFFSET) QueueFree();
+    if (fainted) return;
+
+    if (area.Position.y <= this.Position.y - PIXEL_TOP_OFFSET) {
+      this.SetCollisionMaskBit(0, false);
+      this.velocity.y = JUMP_SPEED;
+      anim.FlipV = true;
+      fainted = true;
+    }
   }
 
   public override void _PhysicsProcess(float delta)
@@ -33,7 +43,7 @@ public class Enemy : KinematicBody2D
     velocity.y += delta * GRAVITY_SCALE;
     MoveAndSlide(velocity, new Vector2(0, -1));
 
-    if (this.Position.x <= PIXEL_LEFT_OFFSET) anim.FlipH = false;
-    else if (this.Position.x >= SCREEN_WIDTH - PIXEL_RIGHT_OFFSET) anim.FlipH = true;
+    if (this.Position.x <= PIXEL_LEFT_OFFSET && fainted == false) anim.FlipH = false;
+    else if (this.Position.x >= SCREEN_WIDTH - PIXEL_RIGHT_OFFSET && fainted == false) anim.FlipH = true;
   }
 }
